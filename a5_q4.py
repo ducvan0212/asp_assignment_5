00f471f873eb0c89a811f93b147691fa82e81144
ndvan <vannd@vccloud.vn>
2015-11-07 13:53:38 -0700

    redo bonus question

--- a5_q4.py
+++ a5_q4.py
@@ -1,19 +1,35 @@
 #script (python)
+
 import gringo
 from gringo import Fun
 from IPython import embed
 
+class Solver:
+    def __init__(self, fpath):
+        self._prg = gringo.Control()
+        self._prg.conf.solve.models = 0 
+        self._prg.load(fpath)
+        self._prg.ground([("base", [])])
+        
 def main():
-    prg = gringo.Control()
-    prg.conf.solve.models = 0
-    prg.load("p.lp")
-    prg.load("q.lp")
-    prg.ground([("base", [])])
-    solutions = prg.solve_iter()
+    p = Solver("p.lp")
+    q = Solver("q.lp")
     
-    for i, sol in enumerate(solutions): 
-        print "Allakhazam #%d: %s" % (i, sol)
+    intermediate = p._prg.solve_iter()
     
+    # with each model in intermediate results
+    for i, model in enumerate(intermediate):
+        # assign each atoms in p model to be true in q
+        for k in model.atoms(gringo.Model.ATOMS):
+            q._prg.assign_external(k, True)   
+        # calculate final solutions, where wizard does his job.
+        final = q._prg.solve_iter()
+        for j, fiso in enumerate(final):
+            print "Allakhazam: %s" % (fiso)
+        # remove assignments.
+        for k in model.atoms(gringo.Model.ATOMS):
+            q._prg.release_external(k)
+        
 #end.
 
 main()
